1be6592bcae5d40de671724d0db6e5e6a7b4c0bb
Sergei O. Udalov <sergei.udalov@gmail.com>
2016-07-04 12:45:00 +0300

    set defaults merged into main script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+###############################################################################
+# Set Defaults
+if [ -z "${PG_USER}" ]; then
+  export PGUSER=postgres
+fi
+
+if [ -z "${PG_PASSWORD}" ]; then
+  export PGPASSWORD=
+fi
+
+if [ -z "${PG_PORT}" ]; then
+  export PG_PORT=5432
+fi
+
+if [ -z "${PG_HOST}" ]; then
+  export PGHOST=db
+fi
+
+###############################################################################
+# Backup
+
 echo "`date`: Dump DB.." >> /var/log/backup.log
 
 PGPASSWORD=$PG_PASSWORD
